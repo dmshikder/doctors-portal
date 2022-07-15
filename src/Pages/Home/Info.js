c62f7ff8e2df1b92627e04e413b73059a9272c97
dmshikder <dmshikder@gmail.com>
2022-07-15 22:59:37 +0600

    infoCard created

---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,17 @@
+import React from "react";
+import clock from "../../assets/icons/clock.svg";
+import marker from "../../assets/icons/marker.svg";
+import phone from "../../assets/icons/phone.svg";
+import InfoCard from "./InfoCard";
+
+const Info = () => {
+  return (
+    <div className="grid grid-cols- lg:grid-cols-3 gap-5 px-12">
+      <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening hours" img={clock}></InfoCard>
+      <InfoCard bgClass="bg-accent" cardTitle="Our Locatins" img={marker}></InfoCard>
+      <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact Us" img={phone}></InfoCard>
+    </div>
+  );
+};
+
+export default Info;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     themes: [
       {
         doctorstheme: {
+          
           primary: "#0FCFEC",
           secondary: "#19D3AE",
           accent: "#3A4256",
